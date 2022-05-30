00f118b131ad6ff9993bbf93794e6a86731780b9
Sachin1138 <99975080+Sachin1138@users.noreply.github.com>
2022-05-30 23:02:56 +0530

    Timer

---
+++ sprint-1/day-4/assignment/timmer/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Timmer from "./components/Timmer";
+function App() {
+  return (
+    <div className="App">
+      <Timmer />
+    </div>
+  );
+}
+
+export default App;
