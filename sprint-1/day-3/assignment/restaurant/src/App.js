974e32bd26cc629697d8f4e327324484f1170ee6
Sachin1138 <99975080+Sachin1138@users.noreply.github.com>
2022-05-31 22:52:33 +0530

    Restaurant completed

---
+++ sprint-1/day-3/assignment/restaurant/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+// import RestaurentCard from "./RestaurentCard";
+import RestaurentDetails from "./RestaurentDetails";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>sachin</h1>
+      {/* <RestaurentCard /> */}
+      <RestaurentDetails />
+    </div>
+  );
+}
+
+export default App;
